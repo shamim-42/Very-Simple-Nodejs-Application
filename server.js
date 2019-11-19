d6ee3edc435191298641a345389439588dc6fabe
shamim42 <shamim.cse.bd@gmail.com>
2019-11-20 00:32:55 +0600

    fresh project

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Bismillah');
+});
+
+app.get('/bis', function (req, res){
+    res.send('Alhamdulillah');
+});
+
+app.get('/alhamdulillah', (req, res) => {
+    res.send("Masha Allah");
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
